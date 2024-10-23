a27c3042ca9e7378f6f7fd3f683a165d6c2f1c00
Michael Bestas <mkbestas@lineageos.org>
2024-10-23 20:23:39 +0300

    hawao: Update to T2SES33.73-23-2-11
    Change-Id: Id6aa536f8251d2449e9c1d3d0fd238e8953ba13e

--- lineage_hawao.mk
+++ lineage_hawao.mk
@@ -21,8 +21,8 @@ PRODUCT_MODEL := moto g42
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-BUILD_FINGERPRINT := "motorola/hawao_g/hawao:13/T2SES33.73-23-2-8/5616a2-1c4eb8:user/release-keys"
+BUILD_FINGERPRINT := "motorola/hawao_g/hawao:13/T2SES33.73-23-2-11/9bc4f-29c9d3:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=hawao_g \
-    PRIVATE_BUILD_DESC="hawao_g-user 13 T2SES33.73-23-2-8 5616a2-1c4eb8 release-keys"
+    PRIVATE_BUILD_DESC="hawao_g-user 13 T2SES33.73-23-2-11 9bc4f-29c9d3 release-keys"
