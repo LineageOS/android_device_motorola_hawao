9800a85e8671a2255d657bac6b8915d62092804b
Michael Bestas <mkbestas@lineageos.org>
2025-01-03 21:26:05 +0200

    hawao: Update to T2SES33.73-23-2-12
    Change-Id: Iaa1ada6b7f94ef86e9a6587e6fbba07a15e5034b

--- lineage_hawao.mk
+++ lineage_hawao.mk
@@ -23,6 +23,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="hawao_g-user 13 T2SES33.73-23-2-11 9bc4f-29c9d3 release-keys" \
-    BuildFingerprint=motorola/hawao_g/hawao:13/T2SES33.73-23-2-11/9bc4f-29c9d3:user/release-keys \
+    BuildDesc="hawao_g-user 13 T2SES33.73-23-2-12 f73e35-2bfcf release-keys" \
+    BuildFingerprint=motorola/hawao_g/hawao:13/T2SES33.73-23-2-12/f73e35-2bfcf:user/release-keys \
     DeviceProduct=hawao_g
