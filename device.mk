c5dd92a8b528ccd0797be7ec92a3994d1c632c23
Michael Bestas <mkbestas@lineageos.org>
2025-09-17 23:23:33 +0300

    hawao: Switch to AIDL samsung NFC HAL
    Change-Id: I968b959d19306b667de2839526e3f0fa80887b71

--- device.mk
+++ device.mk
@@ -27,8 +27,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.oem.fingerprint2.sh \
-    init.mmi.overlay.rc
+    init.mmi.overlay.rc \
+    init.nfc.samsung.rc \
+    init.oem.fingerprint2.sh
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -36,7 +37,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.samsung
+    android.hardware.nfc-service.samsung
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
